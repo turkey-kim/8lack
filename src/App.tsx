7984449cdb989f31c7411a020d38e7f23be98eea
turkey-kim <rlaxmrgml@naver.com>
2023-11-08 19:47:37 +0900

    fix: 글로벌 스타일링 컴포넌트 index.tsx파일로 이전

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import './App.css';
 import styled from 'styled-components';
-import {theme} from './styles/Theme';
-import {ThemeProvider} from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
 import {Outlet} from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <StyledContainer>
-        <Navigation />
-        <SideBar />
-        <Outlet />
-      </StyledContainer>
-    </ThemeProvider>
+    <StyledContainer>
+      <Navigation />
+      <SideBar />
+      <Outlet />
+    </StyledContainer>
   );
 }
 
