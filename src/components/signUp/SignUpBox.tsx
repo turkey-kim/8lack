aaa623913a8c8126cd06dd17d3412b9530da3d6e
turkey-kim <rlaxmrgml@naver.com>
2023-11-08 18:19:20 +0900

    feat: 회원가입 기능 추가

--- src/components/signUp/SignUpBox.tsx
+++ src/components/signUp/SignUpBox.tsx
@@ -1,37 +1,105 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import {theme} from '../../styles/Theme';
 import {useNavigate} from 'react-router';
+import {postSignUp} from '../../api/auth';
+
+interface Props {
+  isError?: string;
+}
 
 const SignUpBox = () => {
   const navigate = useNavigate();
+  const [pwErrorMessage, setPwErrorMessage] = useState('');
+  const [inputs, setInputs] = useState<any>({
+    name: '',
+    id: '',
+    pw: '',
+    pw2: '',
+  });
+  let {name, id, pw, pw2} = inputs;
+
+  useEffect(() => {
+    checkPassword();
+  }, [pw, pw2]);
 
   const goToSignIn = () => {
-    navigate('/signIn');
+    navigate('/signin');
+  };
+
+  const checkPassword = () => {
+    if (pw === pw2) {
+      setPwErrorMessage('');
+    } else if (pw && !pw2) {
+      setPwErrorMessage('');
+    } else if (pw !== pw2) {
+      setPwErrorMessage('비밀번호가 일치하지 않습니다');
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const signUp = async () => {
+    if (pw && pw === pw2) {
+      return await postSignUp(id, pw, name);
+    } else {
+      alert('빈칸을 모두 채워주세요');
+    }
   };
 
   return (
-    <Container>
-      <NavField>
-        <SignInNav onClick={goToSignIn}>로그인</SignInNav>
-        <SignUpNav>회원가입</SignUpNav>
-      </NavField>
-      <FormField>
-        <Label>이름</Label>
-        <Input></Input>
-        <Label>아이디</Label>
-        <Input></Input>
-        <Label>비밀번호</Label>
-        <Input style={{marginBottom: '15px'}}></Input>
-        <Label>비밀번호 확인</Label>
-        <Input></Input>
-        <Submit>회원가입</Submit>
-      </FormField>
-    </Container>
+    <StyledContainer>
+      <StyledNavField>
+        <StyledSignInNav onClick={goToSignIn}>로그인</StyledSignInNav>
+        <StyledSignUpNav>회원가입</StyledSignUpNav>
+      </StyledNavField>
+      <StyledForm>
+        <StyledLabel>이름</StyledLabel>
+        <StyledInput
+          name="name"
+          placeholder="20자 이하의 이름을 입력해주세요"
+          value={name}
+          onChange={onChange}
+        ></StyledInput>
+        <StyledLabel>아이디</StyledLabel>
+        <StyledInput
+          name="id"
+          placeholder="영문으로 이루어진 아이디를 입력해주세요"
+          value={id}
+          onChange={onChange}
+        ></StyledInput>
+        <StyledLabel>비밀번호</StyledLabel>
+        <StyledPwInput
+          name="pw"
+          placeholder="5자리 이상의 비밀번호를 입력해주세요"
+          value={pw}
+          type="password"
+          onChange={onChange}
+          isError={pwErrorMessage}
+        ></StyledPwInput>
+        <StyledLabel style={{marginTop: '0.8rem'}}>비밀번호 확인</StyledLabel>
+        <StyledPwInput
+          name="pw2"
+          placeholder="5자리 이상의 비밀번호를 입력해주세요"
+          value={pw2}
+          type="password"
+          onChange={onChange}
+          isError={pwErrorMessage}
+        ></StyledPwInput>
+        <StyledPwAlarm>{pwErrorMessage}</StyledPwAlarm>
+        <StyledSubmit onClick={signUp}>회원가입</StyledSubmit>
+      </StyledForm>
+    </StyledContainer>
   );
 };
 
-const Container = styled.div`
+const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -43,14 +111,14 @@ const Container = styled.div`
   border-radius: 8px;
 `;
 
-const NavField = styled.div`
+const StyledNavField = styled.div`
   display: flex;
   width: 100%;
 `;
 
-const SignInNav = styled.button`
+const StyledSignInNav = styled.button`
   width: 50%;
-  height: 60px;
+  height: 4rem;
   border: none;
   border-radius: 8px 8px 0 0;
   background-color: ${theme.colors.blue200};
@@ -62,42 +130,52 @@ const SignInNav = styled.button`
   }
 `;
 
-const SignUpNav = styled.button`
+const StyledSignUpNav = styled.button`
   width: 50%;
-  height: 60px;
+  height: 4rem;
   border: none;
   border-radius: 8px 8px 0 0;
   font-size: ${theme.fonts.body1.fontSize};
   font-weight: 800;
 `;
 
-const FormField = styled.div`
+const StyledForm = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 80%;
-  margin-top: 50px;
+  margin-top: 2rem;
 `;
 
-const Label = styled.label`
+const StyledLabel = styled.label`
   align-self: flex-start;
   font-size: ${theme.fonts.body1.fontSize};
   font-weight: 700;
-  margin-bottom: 10px;
+  margin: 1.5rem 0rem 1rem 0rem;
 `;
 
-const Input = styled.input`
+const StyledInput = styled.input`
   width: 100%;
-  height: 40px;
+  height: 2.5rem;
+  padding: 1rem;
   border: 1px solid ${theme.colors.gray500};
   border-radius: 4px;
-  margin-bottom: 30px;
+  outline: none;
 `;
 
-const Submit = styled.button`
+const StyledPwInput = styled.input<Props>`
   width: 100%;
-  height: 60px;
-  margin: 50px 0px;
+  height: 2.5rem;
+  padding: 1rem;
+  border: 1px solid ${props => (props.isError ? theme.colors.error : theme.colors.gray500)};
+  border-radius: 4px;
+  outline: none;
+`;
+
+const StyledSubmit = styled.button`
+  width: 100%;
+  height: 3.5rem;
+  margin: 4rem 0rem;
   border: none;
   border-radius: 8px;
   background-color: ${theme.colors.blue700};
@@ -108,6 +186,12 @@ const Submit = styled.button`
   &:hover {
     background-color: ${theme.colors.blue800};
   }
+`;
+
+const StyledPwAlarm = styled.span`
+  align-self: flex-start;
+  padding: 0.5rem 0rem;
+  color: ${theme.colors.error};
 `;
 
 export default SignUpBox;
