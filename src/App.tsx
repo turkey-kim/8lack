7519914d6347a316c0df0c3f433e9596bd90bce4
turkey-kim <rlaxmrgml@naver.com>
2023-11-09 23:49:58 +0900

    feat: 유저전용페이지 라우팅 구현 및 연동

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import styled from 'styled-components';
 import {Outlet} from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import SideBar from './components/SideBar/SideBar';
+import UserPublicRoute from 'routes/UserPublicRoute';
 
 function App() {
   return (
-    <StyledContainer>
-      <Navigation />
-      <SideBar />
-      <Outlet />
-    </StyledContainer>
+    <UserPublicRoute>
+      <StyledContainer>
+        <Navigation />
+        <SideBar />
+        <Outlet />
+      </StyledContainer>
+    </UserPublicRoute>
   );
 }
 
