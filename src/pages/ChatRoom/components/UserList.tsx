bce32bb4f7cde835f909b127286fcbe0433905ec
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-10 21:10:47 +0900

    feat: 현재 접속 중인 유저 목록 불러오기

---
+++ src/pages/ChatRoom/components/UserList.tsx
@@ -0,0 +1,61 @@
+import React, {useEffect} from 'react';
+import styled from 'styled-components';
+import {IUserList} from './UserList.types';
+import {useSocketContext} from '../../../contexts/SocketContext';
+
+const UserList: React.FC<IUserList> = ({usersMap}) => {
+  const {users, socket} = useSocketContext();
+  const connectedUserIds = users.users;
+
+  useEffect(() => {
+    if (socket) {
+      socket.emit('users', {});
+    }
+  }, [socket]);
+
+  return (
+    <div className="user-list">
+      <h3>전체 유저)</h3>
+      <StyledList>
+        {connectedUserIds.map(user => (
+          <StyledUser key={user}>
+            <span>{user}</span>
+          </StyledUser>
+        ))}
+      </StyledList>
+    </div>
+  );
+};
+
+interface UserStateProps {
+  $online?: boolean;
+}
+
+const StyledList = styled.ul`
+  padding: 0;
+  list-style: none;
+`;
+
+const StyledUser = styled.li<UserStateProps>`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  color: ${({$online, theme}) => ($online ? theme.colors.success : theme.colors.gray500)};
+`;
+
+const StyledImage = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const StatusIndicator = styled.span<UserStateProps>`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: ${({$online, theme}) => ($online ? theme.colors.success : theme.colors.gray500)};
+  margin-left: 5px;
+`;
+
+export default UserList;
