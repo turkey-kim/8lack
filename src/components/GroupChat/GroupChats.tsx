e31c7b5119c21cd8ddbfd080632c3f2af24e3101
Subin Jang <wkdtnqls0506@naver.com>
2023-11-13 00:24:07 +0900

    feat: 메시지 전송시 채팅방 실시간 시간순 정렬

--- src/components/GroupChat/GroupChats.tsx
+++ src/components/GroupChat/GroupChats.tsx
@@ -1,25 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import GroupChat from './GroupChat';
-import {myChatRoom} from 'api/myChatRoom';
 import {StyledContainer} from 'components/PrivateChat/PrivateChats';
 import {Chat} from 'types/chatroom.types';
+import useRealTimeUpdate from 'hooks/useRealTimeUpdate';
 
 export default function GroupChats() {
-  const [myGroupChat, setMyGroupChat] = useState<Chat[]>([]);
   const [sortedChat, setSortedChat] = useState<Chat[]>([]);
+  const {
+    updateQuery: {isLoading, data: realTimeData},
+  } = useRealTimeUpdate();
 
   useEffect(() => {
-    myChatRoom().then(room => {
-      setMyGroupChat(room.chats);
-    });
-  }, []);
-
-  useEffect(() => {
-    const sorted = [...myGroupChat]
-      .filter(room => !room.isPrivate)
-      .sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
-    setSortedChat(sorted);
-  }, [myGroupChat]);
+    if (!isLoading && realTimeData) {
+      const sorted = realTimeData.chats
+        .filter((room: Chat) => !room.isPrivate)
+        .sort((a: Chat, b: Chat) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
+      setSortedChat(sorted);
+    }
+  }, [isLoading, realTimeData]);
 
   return (
     <StyledContainer>
