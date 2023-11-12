63b94aae49bc1eb1dd0a0180fca89d947f16e121
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-12 17:11:22 +0900

    feat: 날짜별로 메시지 그룹화

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,29 @@
+import {PrevMessage, Message} from 'types/chatroom.types';
+
 export const formatMessageDate = (data: Date) => {
   return new Date(data).toLocaleString('ko-KR', {
     hour: 'numeric',
     minute: 'numeric',
     hour12: true,
   });
+};
+
+export const groupMessagesByDate = (prevMessages: PrevMessage) => {
+  const dateGroupedMessages: {[date: string]: Message[]} = {};
+
+  prevMessages.messages.forEach(message => {
+    const messageDate = new Date(message.createdAt).toLocaleDateString('ko-KR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      weekday: 'long',
+    });
+
+    if (!dateGroupedMessages[messageDate]) {
+      dateGroupedMessages[messageDate] = [];
+    }
+    dateGroupedMessages[messageDate].push(message);
+  });
+
+  return dateGroupedMessages;
 };
