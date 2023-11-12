a15c7d270158e924611ec35e71efacf130b3c979
Subin Jang <wkdtnqls0506@naver.com>
2023-11-12 17:55:59 +0900

    feat: React Query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import {Outlet} from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import SideBar from './components/SideBar/SideBar';
 import UserPublicRoute from 'routes/UserPublicRoute';
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <UserPublicRoute>
-      <StyledContainer>
-        <Navigation />
-        <SideBar />
-        <Outlet />
-      </StyledContainer>
-    </UserPublicRoute>
+    <QueryClientProvider client={queryClient}>
+      <UserPublicRoute>
+        <StyledContainer>
+          <Navigation />
+          <SideBar />
+          <Outlet />
+        </StyledContainer>
+      </UserPublicRoute>
+    </QueryClientProvider>
   );
 }
 
