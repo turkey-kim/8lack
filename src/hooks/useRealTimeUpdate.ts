f1c44d521c48c9f10f672094458ea3813794eda8
Subin Jang <wkdtnqls0506@naver.com>
2023-11-13 00:21:35 +0900

    feat: react query hook 생성

---
+++ src/hooks/useRealTimeUpdate.ts
@@ -0,0 +1,13 @@
+import {myChatRoom} from 'api/myChatRoom';
+import {useQuery} from '@tanstack/react-query';
+
+export default function useRealTimeUpdate() {
+  const updateQuery = useQuery({
+    queryKey: ['message'],
+    queryFn: () => myChatRoom(),
+    refetchInterval: 1000,
+    refetchIntervalInBackground: true,
+  });
+
+  return {updateQuery};
+}
