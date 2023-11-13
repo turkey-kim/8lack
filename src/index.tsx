86f8d4ab317111f32a562abe88b28e0db49112a9
turkey-kim <rlaxmrgml@naver.com>
2023-11-13 09:14:43 +0900

    feat: 로그인/회원가입 페이지 논유저라우터 연결

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,7 @@ import SignUp from 'pages/SignUp';
 import ChatRoom from 'pages/ChatRoom';
 import GroupChatList from './pages/GroupChatList/GroupChatList';
 import Modal from 'react-modal';
+import NonUserRoute from 'routes/NonUserRoute';
 
 Modal.setAppElement('#root');
 const router = createBrowserRouter([
@@ -28,8 +29,22 @@ const router = createBrowserRouter([
       {path: '/users', element: <Users />},
     ],
   },
-  {path: '/signin', element: <SignIn />},
-  {path: '/signup', element: <SignUp />},
+  {
+    path: '/signin',
+    element: (
+      <NonUserRoute>
+        <SignIn />
+      </NonUserRoute>
+    ),
+  },
+  {
+    path: '/signup',
+    element: (
+      <NonUserRoute>
+        <SignUp />
+      </NonUserRoute>
+    ),
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
