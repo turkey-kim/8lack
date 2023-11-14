099d1e863d53cdc576a321dafefa613a3898de03
minseokiim <alstj991226@naver.com>
2023-11-15 04:09:11 +0900

    design: 컴포넌트 디자인

--- src/pages/Home/components/IntroSection.tsx
+++ src/pages/Home/components/IntroSection.tsx
@@ -7,61 +7,82 @@ const IntroSection = () => {
     <>
       <StyledLeftSection>
         <StyledWrapper>
-          <StyledText>일대일 & 그룹 채팅방</StyledText>
+          <StyledText>1:1 & 그룹 채팅방</StyledText>
           <StyledSubText>
             친구들과 함께 <br />
             채팅을 시작해보세요.
           </StyledSubText>
           <StyledIntroText>가장 먼저 온 메시지순으로 확인할 수 있어, 편리하게 사용할 수 있답니다!</StyledIntroText>
         </StyledWrapper>
-
-        <StyledImg src={chatimg} />
+        <StyledWrapper>
+          <StyledImg src={chatimg} />
+        </StyledWrapper>
       </StyledLeftSection>
 
       <StyledRightSection>
-        <StyledText>편리한 초대 기능</StyledText>
-        <StyledSubText>
-          대화하고 싶은 친구를
-          <br />
-          검색하고 초대해보세요.
-        </StyledSubText>
-        <StyledIntroText>가장 먼저 온 메시지순으로 확인할 수 있어, 편리하게 사용할 수 있답니다!</StyledIntroText>
+        <StyledWrapper>
+          <StyledImg src={chatimg} />
+        </StyledWrapper>
+        <StyledWrapper>
+          <StyledText>편리한 초대 기능</StyledText>
+          <StyledSubText>
+            대화하고 싶은 친구를
+            <br />
+            검색하고 초대해보세요.
+          </StyledSubText>
+          <StyledIntroText>가장 먼저 온 메시지순으로 확인할 수 있어, 편리하게 사용할 수 있답니다!</StyledIntroText>
+        </StyledWrapper>
       </StyledRightSection>
 
       <StyledLeftSection>
-        <StyledText>나의 채팅목록</StyledText>
-        <StyledSubText>
-          그룹채팅과 1:1 채팅을
-          <br />
-          언제든지 확인하세요.
-        </StyledSubText>
-        <StyledIntroText>가장 먼저 온 메시지순으로 확인할 수 있어, 편리하게 사용할 수 있답니다!</StyledIntroText>
+        <StyledWrapper>
+          <StyledText>나의 채팅목록</StyledText>
+          <StyledSubText>
+            내가 참여하고 있는 채팅을
+            <br />
+            언제든지 확인하세요.
+          </StyledSubText>
+          <StyledIntroText>가장 먼저 온 메시지순으로 확인할 수 있어, 편리하게 사용할 수 있답니다!</StyledIntroText>
+        </StyledWrapper>
+        <StyledWrapper>
+          <StyledImg src={chatimg} />
+        </StyledWrapper>
       </StyledLeftSection>
 
       {/* <>채팅 컴포넌트 추가될 곳</> */}
 
       <StyledRightSection>
-        <StyledText>유저 목록 조회 & 즐겨찾기</StyledText>
-        <StyledSubText>
-          친한 친구를 검색하고 <br />
-          즐겨찾기에 추가해보세요.
-        </StyledSubText>
-        <StyledIntroText>
-          현재 접속중인 친구를 확인하거나, <br />
-          친한 친구를 즐겨찾기하거나. <br />
-          새로운 친구를 만나보세요
-        </StyledIntroText>
+        <StyledWrapper>
+          <StyledImg src={chatimg} />
+        </StyledWrapper>
+        <StyledWrapper>
+          <StyledText>유저 목록 조회 & 즐겨찾기</StyledText>
+          <StyledSubText>
+            친한 친구를 검색하고 <br />
+            즐겨찾기에 추가해보세요.
+          </StyledSubText>
+          <StyledIntroText>
+            현재 접속중인 친구를 확인하거나, <br />
+            친한 친구를 즐겨찾기하거나. <br />
+            새로운 친구를 만나보세요
+          </StyledIntroText>
+        </StyledWrapper>
       </StyledRightSection>
 
       <StyledLeftSection>
-        <StyledText>내 정보</StyledText>
-        <StyledSubText>
-          나에 대해서 <br />더 자유롭게 표현해보세요.
-        </StyledSubText>
-        <StyledIntroText>
-          나만의 개성 넘치는 프로필 이미지와 이름을 설정해보세요. <br />
-          사진 뿐만 아니라, 움직이는 이미지도 사용할 수 있어요!
-        </StyledIntroText>
+        <StyledWrapper>
+          <StyledText>내 정보</StyledText>
+          <StyledSubText>
+            나에 대해서 <br />더 자유롭게 표현해보세요.
+          </StyledSubText>
+          <StyledIntroText>
+            나만의 개성 넘치는 프로필 이미지와 이름을 설정해보세요. <br />
+            사진 뿐만 아니라, 움직이는 이미지도 사용할 수 있어요!
+          </StyledIntroText>
+        </StyledWrapper>
+        <StyledWrapper>
+          <StyledImg src={chatimg} />
+        </StyledWrapper>
       </StyledLeftSection>
     </>
   );
@@ -81,14 +102,13 @@ const StyledWrapper = styled.div`
 
 const StyledImg = styled.img`
   height: 20rem;
-  margin-left: 4rem;
-  margin-top: 10rem;
+  margin: 10rem 0 0 10rem;
 `;
 
 const StyledRightSection = styled.div`
   background-color: ${theme.colors.blue200};
   height: auto;
-  padding: 10rem;
+  padding: 0rem 10rem 10rem 0rem;
   text-align: right;
 `;
 
