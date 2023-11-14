f8e97bb459f679e200687347d693b699160a0e10
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-14 19:56:14 +0900

    Design: 유저 아바타, 이름 표시 되도록 변경

--- src/pages/ChatRoom/components/MessageItem.tsx
+++ src/pages/ChatRoom/components/MessageItem.tsx
@@ -12,9 +12,7 @@ interface MessageItemProps {
 
 const MessageItem: React.FC<MessageItemProps> = ({message, isCurrentUser}) => {
   const usersMap = useRecoilValue(chatRoomUsersSelector) || {};
-
   const user = usersMap[message.userId];
-  // console.log('MessageItem: ', message);
 
   if (message.userId === 'system') {
     return <StyledSystemMessage>{message.text}</StyledSystemMessage>;
@@ -22,9 +20,18 @@ const MessageItem: React.FC<MessageItemProps> = ({message, isCurrentUser}) => {
   return (
     <StyledItem $currentUser={isCurrentUser}>
       <StyledInner $currentUser={isCurrentUser}>
-        {!isCurrentUser && user && <StyledAvatar src={user.picture} alt={user.username} />}
-        <StyledBubble $currentUser={isCurrentUser}>{message.text}</StyledBubble>
-        <StyledDate>{formatMessageDate(message.createdAt)}</StyledDate>
+        <StyledRowSection>
+          <StyledAvatar src={user?.picture} alt={user?.username} />
+        </StyledRowSection>
+        <StyledColSection>
+          <StyledUserName $currentUser={isCurrentUser}>{user?.username}</StyledUserName>
+          <StyledColBottomSection $currentUser={isCurrentUser}>
+            <StyledBubble $currentUser={isCurrentUser}>{message.text}</StyledBubble>
+            <StyledSmallSection>
+              <StyledDate>{formatMessageDate(message.createdAt)}</StyledDate>
+            </StyledSmallSection>
+          </StyledColBottomSection>
+        </StyledColSection>
         <StyledSpacer $currentUser={isCurrentUser} />
       </StyledInner>
     </StyledItem>
@@ -41,33 +48,32 @@ const StyledSystemMessage = styled.div`
 
 const StyledItem = styled.div<{$currentUser: boolean}>`
   display: flex;
-  padding: 10px 0px;
+  padding: 7px 0px;
   width: 100%;
   justify-content: ${({$currentUser}) => ($currentUser ? 'flex-end' : 'flex-start')};
 `;
 
 const StyledBubble = styled.span<{$currentUser: boolean}>`
-  word-break: break-all;
   background-color: ${({theme, $currentUser}) => ($currentUser ? theme.colors.blue700 : theme.colors.gray300)};
   color: ${({theme, $currentUser}) => ($currentUser ? theme.colors.white : theme.colors.blueBg3)};
   padding: 0.8em 1.1rem;
-  border-radius: 1rem;
+  border-radius: 25px;
+  border-top-left-radius: ${({theme, $currentUser}) => ($currentUser ? 25 : 1)}px;
+  border-top-right-radius: ${({theme, $currentUser}) => ($currentUser ? 1 : 25)}px;
   max-width: 500px;
   overflow-wrap: break-word;
-`;
-
-const StyledInner = styled.div<{$currentUser: boolean}>`
-  display: flex;
-  align-items: center;
-  gap: 0.65rem;
-  ${({$currentUser}) => ($currentUser ? 'flex-direction: row-reverse;' : '')}
+  word-break: break-all;
+  font-size: 0.97rem;
+  font-weight: 400;
 `;
 
 const StyledAvatar = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 34px;
+  height: 34px;
   border-radius: 50%;
   overflow: hidden;
+  background-color: ${({theme}) => theme.colors.white};
+  border: 1px solid ${({theme}) => theme.colors.gray300};
 `;
 
 const StyledDate = styled.span`
@@ -75,8 +81,49 @@ const StyledDate = styled.span`
   height: 100%;
   display: flex;
   align-items: flex-end;
-  font-size: 0.78rem;
+  font-size: 0.72rem;
   color: ${({theme}) => theme.colors.gray700};
+`;
+
+const StyledInner = styled.div<{$currentUser: boolean}>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  ${({$currentUser}) => ($currentUser ? 'flex-direction: row-reverse;' : '')}
+`;
+
+const StyledUserName = styled.span<{$currentUser: boolean}>`
+  font-size: 0.8rem;
+  color: ${({theme}) => theme.colors.gray700};
+  display: flex;
+  justify-content: ${({$currentUser}) => ($currentUser ? 'flex-end' : 'flex-start')};
+  padding: 0.3rem 0 0.5rem;
+  height: 100%;
+`;
+
+const StyledRowSection = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  height: 100%;
+`;
+
+const StyledColSection = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledColBottomSection = styled.div<{$currentUser: boolean}>`
+  flex: 2;
+  display: flex;
+  ${({$currentUser}) => ($currentUser ? 'flex-direction: row-reverse;' : '')}
+`;
+
+const StyledSmallSection = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
 const StyledSpacer = styled.div<{$currentUser: boolean}>`
