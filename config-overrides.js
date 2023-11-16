c3c82b80365d7a0080f936c0ec58d6b413523799
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2023-11-16 20:02:54 +0900

    refactor: image-webpack-loader 플러그인 추가

--- config-overrides.js
+++ config-overrides.js
@@ -3,54 +3,87 @@ const {override: configOverrides} = require('customize-cra');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
+const ImageMinimizerPlugin = require('image-webpack-loader');
 
 const isEnvProductionProfile = process.env.NODE_ENV === 'production' && process.env.PROFILE === 'true';
 
-module.exports = configOverrides((config, env) => {
-  if (env === 'production') {
-    config.optimization.minimizer = [
-      new TerserPlugin({
-        terserOptions: {
-          parse: {
-            ecma: 8,
+module.exports = configOverrides(
+  (config, env) => {
+    if (env === 'production') {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              comparisons: false,
+              inline: 2,
+              drop_console: true,
+            },
+            mangle: {
+              safari10: true,
+            },
+            keep_classnames: isEnvProductionProfile,
+            keep_fnames: isEnvProductionProfile,
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
           },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            comparisons: false,
-            inline: 2,
-            drop_console: true,
+        }),
+      ];
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          algorithm: 'gzip',
+          test: /\.(js|css|html|svg)$/,
+          threshold: 10240,
+          minRatio: 0.8,
+        }),
+      );
+      config.plugins.push(
+        new BrotliPlugin({
+          test: /\.(js|css|html|svg)$/,
+          threshold: 10240,
+          minRatio: 0.8,
+        }),
+      );
+      config.devtool = isEnvProductionProfile ? 'source-map' : false;
+    }
+
+    return config;
+  },
+  addWebpackModuleRule({
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+      {
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            progressive: true,
+            quality: 65,
           },
-          mangle: {
-            safari10: true,
+          optipng: {
+            enabled: true,
           },
-          keep_classnames: isEnvProductionProfile,
-          keep_fnames: isEnvProductionProfile,
-          output: {
-            ecma: 5,
-            comments: false,
-            ascii_only: true,
+          pngquant: {
+            quality: [0.65, 0.9],
+            speed: 4,
+          },
+          gifsicle: {
+            interlaced: false,
+          },
+          webp: {
+            quality: 75,
           },
         },
-      }),
-    ];
-    config.plugins.push(
-      new CompressionWebpackPlugin({
-        algorithm: 'gzip',
-        test: /\.(js|css|html|svg)$/,
-        threshold: 10240,
-        minRatio: 0.8,
-      }),
-    );
-    config.plugins.push(
-      new BrotliPlugin({
-        test: /\.(js|css|html|svg)$/,
-        threshold: 10240,
-        minRatio: 0.8,
-      }),
-    );
-    config.devtool = isEnvProductionProfile ? 'source-map' : false;
-  }
-
-  return config;
-});
+      },
+    ],
+  }),
+);
